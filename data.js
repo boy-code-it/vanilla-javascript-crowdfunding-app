4a2194a17c3a92000e9fcbaa2f77d527089ad19b
Landon Sanders <stockmicroblog@gmail.com>
2016-04-22 03:51:35 +0000

    Do initial commit

---
+++ data.js
@@ -0,0 +1,68 @@
+var data = {
+        results: [{
+            id: 0,
+            title: 'Help',
+            date: Date.now(),
+            img: 'http://placehold.it/300',
+            info: 'For shame, I am in need of help. Something horrible has happened -- I lost my job.',
+            now: '10.00',
+            then: '50000.00',
+            donations: [{
+                amt: '10.00',
+                donor: 'Jill Doe'
+            }]
+        },
+        {
+            id: 1,
+            title: 'Need money for a Project',
+            date: Date.now(),
+            img: 'http://placehold.it/300',
+            info: 'I am working on something super and top secret. I got the plans. All I need is the dough. This thing is going to change the entire world.',
+            now: '10.00',
+            then: '50000.00',
+            donations: [{
+                amt: '10.00',
+                donor: 'Jill Doe'
+            }]
+        },
+        {
+            id: 2,
+            title: 'Need help with rent',
+            date: Date.now(),
+            img: 'http://placehold.it/300',
+            info: 'I would appreciate anything you can do. I can\'t pay my rent this month. I guess I ate too much lobster and steak lateley. Help me out, please and thanks.',
+            now: '10.00',
+            then: '50000.00',
+            donations: [{
+                amt: '10.00',
+                donor: 'Jill Doe'
+            }]
+        },
+        {
+            id: 3,
+            title: 'Greatest Shower Ever!',
+            date: Date.now(),
+            img: 'http://placehold.it/300',
+            info: 'I want to throw the best baby shower ever! Any donations are worth it! It is going to be the greatest shower, of all',
+            now: '10.00',
+            then: '50000.00',
+            donations: [{
+                amt: '10.00',
+                donor: 'Jill Doe'
+            }]
+        },
+        {
+            id: 4,
+            title: 'Trying to get Through',
+            date: Date.now(),
+            img: 'http://placehold.it/300',
+            info: 'I need a future. I am trying to get through college. How can you help?',
+            now: '10.00',
+            then: '50000.00',
+            donations: [{
+                amt: '10.00',
+                donor: 'Jill Doe'
+            }]
+        },
+    ]};
+            
---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+            function home() {
+                var text = jQuery('#home-template').html();
+                
+                var template = Handlebars.compile(text);
+                
+                var content = template(data);
+                
+                jQuery('#app').html(content);
+            }
+            
+            function page(number) {
+                var text = jQuery('#page-template').html();
+            
+                var template = Handlebars.compile(text);
+                
+                var content = template(data.results[number]);
+                
+                jQuery('#app').html(content);
+            }
+        
+
+function donate(number) {
+    var text = jQuery('#donate-template').html();
+
+    var template = Handlebars.compile(text);
+    
+    var content = template(data.results[number]);
+    
+    jQuery('#app').html(content);
+}
+
+var routes = {}, el;
+
+function addRoute(path, callback) {
+    routes[path] = callback;
+}
+
+addRoute('', home);
+addRoute('#home', home);
+addRoute('#page', page);
+addRoute('#donate', donate);
+
+function displayRoute() {
+    el = el || document.getElementById('app'),
+        route = window.location.hash.split('/');
+    
+    if (route[0] === '#page') {
+        routes[route[0]](parseInt(route[1], 10));
+    }
+    
+    if (route[0] === '#home' || route[0] === '') {
+        routes[route[0]]();
+    }
+    
+    if (route[0] === '#donate') {
+        routes[route[0]]((parseInt(route[1], 10)));
+    }
+    
+    console.log('displayRoute()');
+}
+        
+window.addEventListener('load', displayRoute, false);
+window.addEventListener('hashchange', displayRoute, false);
+
+function addDonation(e) {
+    e.preventDefault();
+    var name = e.target['name'].value,
+        amount = e.target['amount'].value,
+        id = parseInt(e.target['id'].value, 10);
+        
+        data.results[id].donations.push({donor: name, amt: amount, date: Date.now()});
+        
+        console.log(data.results[id].donations);
+        
+        window.location.href = '#page/' + id;
+}
